c5eec3faa048c85639f72bc88e4c894ac59a48a9
aleksandr <aleksandr_drobnov@epam.com>
2021-11-18 22:17:42 +0300

    FastAPI microblog
    -add get_post_list and create_post endpoints

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-from core.database import Base
-from microblog.models import Post
+from fastapi import FastAPI, APIRouter, Response, Request
+
+from microblog import blog
+from core.database import SessionLocal
+
+app = FastAPI()
+app.include_router(blog.router, prefix="/blog")
+
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
+
